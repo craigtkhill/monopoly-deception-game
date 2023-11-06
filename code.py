e8bdcfc4c896fa5d7ac0caad82ca18c67b02cf88
Craig <craigthomashill@gmail.com>
2023-11-06 12:15:44 +0000

    add monotonic timer

--- code.py
+++ code.py
@@ -12,22 +12,23 @@ BUTTON_DEBOUNCE = 0.2
 
 # Colors for players
 ALL_COLORS = [
-    (0, 0, 255), (0, 255, 0), (255, 255, 0),
-    (0, 255, 255), (255, 0, 255), (255, 0, 0),
-    (255,255,255), (255, 50, 0)
+    (0, 0, 255),
+    (0, 255, 0),
+    (255, 255, 0),
+    (0, 255, 255),
+    (255, 0, 255),
+    (255, 0, 0),
+    (255, 255, 255),
+    (255, 50, 0),
 ]
-
-# Variables
-num_players = MAX_PLAYERS
-current_round = 0
-current_pixel = 0
-round_colors = []
 
 # Initialize brightness
 cp.pixels.brightness = BRIGHTNESS
 
+
 def play_tone(frequency, duration):
     cp.play_tone(frequency, duration)
+
 
 def display_colors():
     for i in range(num_players):
@@ -35,6 +36,7 @@ def display_colors():
     for i in range(num_players, len(cp.pixels)):
         cp.pixels[i] = (0, 0, 0)
     cp.pixels.show()
+
 
 def flash_color(color, times):
     for _ in range(times):
@@ -45,65 +47,76 @@ def flash_color(color, times):
         cp.pixels.show()
         time.sleep(FLASH_DELAY)
 
-def wait_for_button():
-    print("Waiting for button press...")
-    while not cp.button_a and not cp.button_b:
+
+# Main game loop
+while True:
+    # Variables that need to be reset at the start of each game
+    start_time = time.monotonic()
+    num_players = MAX_PLAYERS
+    current_round = 0
+    current_pixel = 0
+    round_colors = []
+
+    # Setup phase
+    display_colors()
+
+    # Game start phase
+    while True:
+        if cp.button_a:
+            num_players = max(1, num_players - 1) if num_players > 1 else MAX_PLAYERS
+            display_colors()
+        elif cp.button_b:
+            print("Game starting!")
+            break
         time.sleep(0.1)
-    print("Button pressed!")
-    time.sleep(BUTTON_DEBOUNCE)  # debounce
 
-# Setup phase
-display_colors()
-
-# Game start phase
-while True:
-    if cp.button_a:
-        num_players = max(1, num_players - 1) if num_players > 1 else MAX_PLAYERS
-        display_colors()
-    elif cp.button_b:
-        print("Game starting!")
-        break
-    time.sleep(0.1)
-
-# Clear the pixels before starting the game
-cp.pixels.fill((0, 0, 0))
-cp.pixels.show()
-
-# Game phase
-colors = ALL_COLORS[:num_players]
-while current_round < NUM_ROUNDS:
-    color = random.choice(colors)
-    round_colors.append(color)
-
-    interval = random.randint(*TIMER_INTERVAL)
-
-    cp.pixels[current_pixel] = color
+    # Clear the pixels before starting the game
+    cp.pixels.fill((0, 0, 0))
     cp.pixels.show()
-    time.sleep(interval)
 
-    current_pixel += 1 if random.random() >= 0.1 or current_pixel == 0 else -1
+    # Game phase
+    colors = ALL_COLORS[:num_players]
+    while current_round < NUM_ROUNDS:
+        time.sleep(1)
+        color = random.choice(colors)
+        round_colors.append(color)
+        interval = random.randint(*TIMER_INTERVAL)
 
-    if current_pixel >= len(cp.pixels):
-        play_tone(880, 1)
-        current_pixel = 0
-        current_round += 1
+        cp.pixels[current_pixel] = color
+        cp.pixels.show()
+        time.sleep(interval)
 
-        if current_round <= NUM_ROUNDS:
-            color_counts = {color: round_colors.count(color) for color in colors}
-            max_count = max(color_counts.values())
-            most_common_colors = [color for color, count in color_counts.items() if count == max_count]
+        current_pixel += 1 if random.random() >= 0.1 or current_pixel == 0 else -1
 
-            winning_color = random.choice(most_common_colors)
-            print(f"Round {current_round} winner's color is {winning_color}!")
+        if current_pixel >= len(cp.pixels):
+            play_tone(220, 1)
+            current_pixel = 0
+            current_round += 1
 
-            print("Flashing winning color until button press...")
-            while not cp.button_a and not cp.button_b:
-                flash_color(winning_color, 1)
+            if current_round < NUM_ROUNDS:
+                color_counts = {color: round_colors.count(color) for color in colors}
+                max_count = max(color_counts.values())
+                most_common_colors = [
+                    color for color, count in color_counts.items() if count == max_count
+                ]
+                winning_color = random.choice(most_common_colors)
 
-            round_colors = []
-            cp.pixels.fill((0, 0, 0))
-            cp.pixels.show()
+                print(f"Round {current_round} winner's color is {winning_color}!")
+                print("Flashing winning color until button press...")
 
-# Game over phase
-while True:
-    cp.pixels.fill((255, 0, 0))
+                while not cp.button_a and not cp.button_b:
+                    flash_color(winning_color, 1)
+
+                round_colors = []
+                cp.pixels.fill((0, 0, 0))
+                cp.pixels.show()
+
+    # Game over phase
+    while True:
+        cp.pixels.fill((255, 0, 0))
+        cp.pixels.show()
+        time.sleep(0.1)
+
+        if cp.button_a or cp.button_b:
+            print("Restarting game!")
+            break  # Exit the game over phase and restart the game
